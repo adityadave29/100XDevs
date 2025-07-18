3e8b02387b3cf60edbb583e6b479c4af75f2fa51
Aditya Dave <cool2912004@gmail.com>
2025-07-18 22:31:18 +0530

    18-jul

--- JS_BASICS/nodejs/express.js
+++ JS_BASICS/nodejs/express.js
@@ -5,3 +5,15 @@
 
 // deploy website on port 443
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
