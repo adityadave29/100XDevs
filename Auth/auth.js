386b00d28770ae98586a5d24abe03150581ee0da
Aditya Dave <cool2912004@gmail.com>
2025-07-23 11:37:43 +0530

    23-july

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,37 +1,71 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const JWT_SECRET = "random";
 const app = express();
-const port= 3000;
+const port= 3001;
+
+
 app.use(express.json());
+app.use(cors());
 
 const users = [];
 app.post("/signup",(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
-    res.send("User signed up successfully");
+    users.push({
+        username: username,
+        password: password
+    })
+    res.json({
+        message: "User registered successfully",
+    })
 });
 app.post("/signin",(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
 
     let foundUser = null;
+
     for(let i=0;i<users.length;i++){
-        if(users[i].username==username && users[i].password==password){
-            foundUser=users[i];
+        if(users[i].username==username && users[i].password == password){
+            foundUser = users[i];
             break;
         }
     }
-    if(foundUser){
-        const token = jwt.sign({username: username},JWT_SECRET);
-        foundUser.token=token;
-        res.json({
-            token:token
+    if(!foundUser){
+        return res.json({
+            message: "Invalid username or password"
         });
     }
+    else{
+        const token = jwt.sign({
+            username: users[i].username,
+        },JWT_SECRET);
+        res.json({
+            token:token,
+        })
+    }
+});
 
-    console.log(users);
+app.get("/me",(req,res)=>{
+    const token = req.headers.token;
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if(decodedData.username){
+        let foundUser=null;
+        for(let i=0;i<users.length;i++){
+            if(users[i].username===decodedData.username){
+                foundUser = users[i];
+                break;
+            }
+        }
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    }
 
 });
 
-app.listen(`listing to port number : ${port}`);
+app.listen(port);
